4591217c68472a60fb9bb8cc88b34ddcf69ab760
MoonFruit <dkmoonfruit@gmail.com>
2020-11-19 11:20:37 +0800

    bug fix

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,7 +1,9 @@
 class Sshpass < Formula
+  desc "Non-interactive ssh password auth"
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  license "GPL-2.0"
 
   def install
     system "./configure", "--disable-debug",
