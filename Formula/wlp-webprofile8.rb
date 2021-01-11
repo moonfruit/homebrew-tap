4e7ae54fdefd47ddd1bdefd656721f5054a36e39
MoonFruit <dkmoonfruit@gmail.com>
2021-01-11 15:40:33 +0800

    wlp-webprofile8: add livecheck

--- Formula/wlp-webprofile8.rb
+++ Formula/wlp-webprofile8.rb
@@ -4,6 +4,11 @@ class WlpWebprofile8 < Formula
   url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/20.0.0.12/wlp-webProfile8-20.0.0.12.zip"
   sha256 "c49069e1e33bc253ff69f507594156e3cd267492ef8b9dfb24d2a975c78a900c"
 
+  livecheck do
+    url "https://www.ibm.com/support/pages/node/6250961#asset/runtimes-wlp-javaee8"
+    regex(/wlp-webProfile8[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
   bottle :unneeded
 
   depends_on "openjdk"
