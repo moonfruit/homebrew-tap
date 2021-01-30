ae0e2c0a9e97fd87d0341bb4cdf154bb4edc83e2
MoonFruit <dkmoonfruit@gmail.com>
2021-01-30 11:10:39 +0800

    sshpass 1.09

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
   desc "Non-interactive ssh password auth"
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.07/sshpass-1.07.tar.gz"
-  sha256 "986973c8dd5d75ff0febde6c05c76c6d2b5c4269ec233e5518f14f0fd4e4aaef"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
   license "GPL-2.0-or-later"
 
   def install
