dc0aadcb8efe4200325828bd906a8d2b1e4005c0
MoonFruit <dkmoonfruit@gmail.com>
2021-03-22 14:27:47 +0800

    wlp-webprofile8 21.0.0.3

--- Formula/wlp-webprofile8.rb
+++ Formula/wlp-webprofile8.rb
@@ -1,8 +1,8 @@
 class WlpWebprofile8 < Formula
   desc "IBM WebSphere Liberty Web Profile 8"
   homepage "https://www.ibm.com/cloud/websphere-liberty"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/21.0.0.2/wlp-webProfile8-21.0.0.2.zip"
-  sha256 "dc42a7a4af3af15edb103ae32a28d5e04e4f12e0df1b5b2edd92805f50a0ab6a"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/21.0.0.3/wlp-webProfile8-21.0.0.3.zip"
+  sha256 "a1aeb4e6554f53a181a1d35b8389626d89505dec3abf698fd1bdd77067ef6dff"
 
   livecheck do
     url "https://www.ibm.com/support/pages/websphere-liberty-developers"
