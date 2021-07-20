432751a24b690a470463bbba6d88752227b093f9
MoonFruit <dkmoonfruit@gmail.com>
2021-07-20 21:09:48 +0800

    Update wlp-webprofile8

--- Formula/wlp-webprofile8.rb
+++ Formula/wlp-webprofile8.rb
@@ -1,5 +1,5 @@
 class WlpWebprofile8 < Formula
-  desc "Implementation of Jakarta EE Web Profile 8"
+  desc "Jakarta EE and MicroProfile application server (Jakarta EE Web Profile 8)"
   homepage "https://www.ibm.com/cloud/websphere-liberty"
   url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/21.0.0.7/wlp-webProfile8-21.0.0.7.zip"
   sha256 "89628ec386a1c43a0fba8dc6b94f96c9eb799975a6edbb9f95ece7c24d2981af"
@@ -16,11 +16,30 @@ class WlpWebprofile8 < Formula
   def install
     rm_rf Dir["bin/**/*.bat"]
 
-    prefix.install %w[CHANGES.TXT Copyright.txt README.TXT]
+    prefix.install_metafiles
     libexec.install Dir["*"]
+    (bin/"wlp-webprofile8").write_env_script "#{libexec}/bin/server",
+                                             Language::Java.overridable_java_home_env
+  end
+
+  def caveats
+    <<~EOS
+      The home of Websphere Liberty Jakarta EE Web Profile 8 is:
+        #{opt_libexec}
+    EOS
   end
 
   test do
-    system libexec/"bin/server", "version"
+    ENV["WLP_USER_DIR"] = testpath
+
+    begin
+      system bin/"wlp-webprofile8", "start"
+      assert_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    ensure
+      system bin/"wlp-webprofile8", "stop"
+    end
+
+    refute_predicate testpath/"servers/.pid/defaultServer.pid", :exist?
+    assert_match "<feature>webProfile-8.0</feature>", (testpath/"servers/defaultServer/server.xml").read
   end
 end
