821f747592e0c56303123ec655782a9de5a7aeb8
MoonFruit <dkmoonfruit@gmail.com>
2023-10-18 09:46:43 +0800

    Remove relpath from uutils-selected

--- Formula/uutils-selected.rb
+++ Formula/uutils-selected.rb
@@ -23,7 +23,7 @@ class UutilsSelected < Formula
     system "gmake", "install",
            "PREFIX=#{prefix}",
            "PROFILE=release", "MULTICALL=y",
-           "UTILS=dircolors hashsum realpath relpath"
+           "UTILS=dircolors hashsum realpath"
 
     bin.install_symlink "coreutils" => "md5sum"
     bin.install_symlink "coreutils" => "sha1sum"
