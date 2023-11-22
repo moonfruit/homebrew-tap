72b008e3cae36c358a9551c5c78c96294a534aed
MoonFruit <dkmoonfruit@gmail.com>
2023-11-22 10:02:06 +0800

    font-jetbrains-mono-nerd-light 3.1.0

--- Casks/font-jetbrains-mono-nerd-light.rb
+++ Casks/font-jetbrains-mono-nerd-light.rb
@@ -1,6 +1,6 @@
 cask "font-jetbrains-mono-nerd-light" do
-  version "3.0.2"
-  sha256 "4991258b7c97071238a7459f0d3bf81a893ae7b0c849dbc47ad52833a8db7f55"
+  version "3.1.0"
+  sha256 "4043a2c17c716f15be32ac2b79c201771fc1866d89848d05f143383acfe35330"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/JetBrainsMono.tar.xz"
   name "JetBrains Mono Nerd Font"
