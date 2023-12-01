45c0a7bed4832aa5aa89850b944e1065929aba26
MoonFruit <dkmoonfruit@gmail.com>
2023-12-01 18:06:48 +0800

    impl: bug fix

--- Formula/impl.rb
+++ Formula/impl.rb
@@ -4,7 +4,7 @@ class Impl < Formula
   url "https://github.com/josharian/impl/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "1a5e02e396c4d60811780af4de44ba25ff96d851d240f8b39c2086def5ecfd6d"
   license "MIT"
-  head "https://github.com/josharian/impl.git"
+  head "https://github.com/josharian/impl.git", branch: "main"
 
   depends_on "go"
 
