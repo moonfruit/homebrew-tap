a443378c240739f79b4e7e3a1cac0680379f6e79
MoonFruit <dkmoonfruit@gmail.com>
2024-07-22 14:16:29 +0800

    gost@3 3.0.0-nightly.20240719

--- Formula/gost@3.rb
+++ Formula/gost@3.rb
@@ -1,9 +1,9 @@
 class GostAT3 < Formula
   desc "Go simple tunnel"
   homepage "https://gost.run/"
-  url "https://github.com/go-gost/gost/archive/refs/tags/v3.0.0-nightly.20240715.tar.gz"
-  version "3.0.0-nightly.20240715"
-  sha256 "577d8162fae24a70bd5c1281fdc7b16373eb76ce32e4cbe789df22eaf545e331"
+  url "https://github.com/go-gost/gost/archive/refs/tags/v3.0.0-nightly.20240719.tar.gz"
+  version "3.0.0-nightly.20240719"
+  sha256 "d546ba40657d1cc3ac7e827b4f11b6e454eff9bca6e211509d129a338bb9518f"
   license "MIT"
   head "https://github.com/go-gost/gost.git", branch: "master"
 
