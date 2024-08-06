c507ff8336f4d30767f8cb9bacd40765d70a8b6d
MoonFruit <dkmoonfruit@gmail.com>
2024-08-06 14:18:35 +0800

    doge: 0.2.7 (new)

---
+++ Formula/doge.rb
@@ -0,0 +1,37 @@
+class Doge < Formula
+  desc "Reviving A command-line DNS client"
+  homepage "https://dns.lookup.dog/"
+  url "https://github.com/Dj-Codeman/doge/archive/refs/tags/v0.2.7.tar.gz"
+  sha256 "7049e2ccd6907f4f4222b8ea84160d65b57aadbbee9498da353a00c576bc647e"
+  license "EUPL-1.2"
+
+  depends_on "pandoc" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@3"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    bash_completion.install "completions/doge.bash" => "doge"
+    zsh_completion.install "completions/doge.zsh" => "_doge"
+    fish_completion.install "completions/doge.fish"
+
+    args = %w[
+      --standalone
+      --from=markdown
+      --to=man
+    ]
+    system "pandoc", *args, "man/doge.1.md", "-o", "doge.1"
+    man1.install "doge.1"
+  end
+
+  test do
+    output = shell_output("#{bin}/doge dns.google A --seconds --color=never")
+    assert_match(/^A\s+dns\.google\.\s+\d+\s+8\.8\.4\.4/, output)
+    assert_match(/^A\s+dns\.google\.\s+\d+\s+8\.8\.8\.8/, output)
+  end
+end
