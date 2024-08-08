156aab1a22c80cc16e6790e70e5152ce11f8c800
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2024-08-08 02:40:07 +0000

    doge: update 0.2.7 bottle.

--- Formula/doge.rb
+++ Formula/doge.rb
@@ -6,10 +6,10 @@ class Doge < Formula
   license "EUPL-1.2"
 
   bottle do
-    rebuild 1
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, ventura:      "96d60875ad1f70a102fbff78bbaf9bff0b157ef7f2c7c5aac88cce6be7749ff9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2e67ed413f0fa8a9a8997475b30c91dee9e9b2099f888f82b2eef9df98f1fee1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "62b35f3ce75f465c4a74f45f17745531721f5ea73ba9e201b6f381bb21a66352"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b827848c454b7c0e55dce9f767eb3ff758266b3b9d6996aebf9d3886601f3eaa"
   end
 
   depends_on "pandoc" => :build
