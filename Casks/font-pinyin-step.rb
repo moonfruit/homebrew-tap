377556a1213ba021b85bdb3a6070092a1aa58b3c
MoonFruit <dkmoonfruit@gmail.com>
2024-08-09 10:20:49 +0800

    font-pinyin-step 1.10.1

--- Casks/font-pinyin-step.rb
+++ Casks/font-pinyin-step.rb
@@ -1,6 +1,6 @@
 cask "font-pinyin-step" do
-  version "1.9.1"
-  sha256 "3e01cba09886acdd28c707d9f185ed57f001631bbe21daa00c4cd42b1b34a4bc"
+  version "1.10.1"
+  sha256 "45843adb0232eb2f6c0ab712847ec8b3c7ad893a59a110a1907589bba1f02e69"
 
   url "https://github.com/jaywcjlove/pinyin-font/releases/download/v#{version}/pinyin-step.ttf"
   name "font-pinyin-step"
