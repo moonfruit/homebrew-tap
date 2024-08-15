2042a811c36a7563059ecf57999ae22773a2608d
MoonFruit <dkmoonfruit@gmail.com>
2024-08-15 12:14:43 +0800

    wlp-webprofile8 24.0.0.8

--- Formula/wlp-webprofile8.rb
+++ Formula/wlp-webprofile8.rb
@@ -1,8 +1,8 @@
 class WlpWebprofile8 < Formula
   desc "Jakarta EE and MicroProfile application server (Jakarta EE Web Profile 8)"
   homepage "https://www.ibm.com/cloud/websphere-liberty"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/24.0.0.7/wlp-webProfile8-24.0.0.7.zip"
-  sha256 "9123514ff24bd92ba94c2a85d8d5d6ec029b35fceb9245c388d8680297e32bf3"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/24.0.0.8/wlp-webProfile8-24.0.0.8.zip"
+  sha256 "6fa3afd903244c6decc64dccc87e1a81f9d04b1e97c9d4282208f68fe0eec436"
 
   livecheck do
     url "https://www.ibm.com/support/pages/websphere-liberty-developers"
