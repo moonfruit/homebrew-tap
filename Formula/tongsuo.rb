13286260a993285c2ae142a975a6bc80310997dd
MoonFruit <dkmoonfruit@gmail.com>
2024-09-06 19:44:49 +0800

    tongsuo: support build on linux
    Closes #149.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/tongsuo.rb
+++ Formula/tongsuo.rb
@@ -7,12 +7,35 @@ class Tongsuo < Formula
 
   keg_only "conflicts with openssl"
 
+  depends_on "ca-certificates"
+
   def install
-    system "./config", "--prefix=#{prefix}", "--release", "enable-ntls"
+    openssldir.mkpath
+    system "./config", "--prefix=#{prefix}", "--openssldir=#{openssldir}", "--libdir=lib", "--release", "enable-ntls"
     system "perl", "configdata.pm", "--dump"
     system "make"
     system "make", "install"
     system "make", "test"
+  end
+
+  def openssldir
+    etc/"tongsuo"
+  end
+
+  def post_install
+    rm(openssldir/"cert.pem") if (openssldir/"cert.pem").exist?
+    openssldir.install_symlink Formula["ca-certificates"].pkgetc/"cert.pem"
+  end
+
+  def caveats
+    <<~EOS
+      A CA file has been bootstrapped using certificates from the system
+      keychain. To add additional certificates, place .pem files in
+        #{openssldir}/certs
+
+      and run
+        #{opt_bin}/c_rehash
+    EOS
   end
 
   test do
