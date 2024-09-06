8d1b1224c84a39530f6700eed94ad9b3be7b7174
MoonFruit <dkmoonfruit@gmail.com>
2024-09-06 18:02:01 +0800

    sshpass: rebuild 1
    Closes #147.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -5,6 +5,10 @@ class Sshpass < Formula
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
   license "GPL-2.0-or-later"
 
+  bottle do
+    rebuild 1
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
