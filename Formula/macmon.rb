76ca320142c783ed63db91e6e129d5bacfe29ba6
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2024-10-15 02:23:19 +0000

    macmon: add 0.3.0 bottle.

--- Formula/macmon.rb
+++ Formula/macmon.rb
@@ -5,6 +5,11 @@ class Macmon < Formula
   sha256 "4c1ae56cda1566a902843db72c5a3dd9612abb4500aa9d2ad022c4c2bf0b07cd"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fde7f6673926f9776a4e688fe04332dcabe8db7f164af32c1e62702f9695071d"
+  end
+
   depends_on "rust" => :build
   depends_on arch: :arm64
   depends_on :macos
