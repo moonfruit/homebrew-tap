92712e7787e796509d7c6003af6f8aafcc47c88f
MoonFruit <dkmoonfruit@gmail.com>
2025-02-10 10:00:20 +0800

    gost@3: 3.0.0

---
+++ Formula/gost@3.rb
@@ -0,0 +1,33 @@
+class GostAT3 < Formula
+  desc "Go simple tunnel"
+  homepage "https://gost.run/"
+  url "https://github.com/go-gost/gost/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "1aab7bf19e616f365758a3b49ab49b6ddee0952f4530c85d0c8ec90aa0e81056"
+  license "MIT"
+  head "https://github.com/go-gost/gost.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"gost"), "./cmd/gost"
+    prefix.install "README_en.md"
+  end
+
+  test do
+    bind_address = "127.0.0.1:#{free_port}"
+    fork do
+      exec "#{bin}/gost -L #{bind_address}"
+    end
+    sleep 2
+    output = shell_output("curl -I -x #{bind_address} https://github.com")
+    assert_match %r{HTTP/\d+(?:\.\d+)? 200}, output
+    assert_match(/Server: GitHub.com/i, output)
+  end
+end
