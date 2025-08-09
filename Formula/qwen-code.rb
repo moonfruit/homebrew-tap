0aee8f7cb1a0a6679d43bdcf8ef96f94a230ed52
MoonFruit <dkmoonfruit@gmail.com>
2025-08-09 17:05:30 +0800

    qwen-code: 0.0.5 (new formula)

---
+++ Formula/qwen-code.rb
@@ -0,0 +1,18 @@
+class QwenCode < Formula
+  desc "AI-powered command-line workflow tool for developers"
+  homepage "https://github.com/QwenLM/qwen-code"
+  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.0.5.tgz"
+  sha256 "1b92595e24bd8648eff58b3c5e08d3b082536b9f3cc5a3beee64d0c543a612ef"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/qwen --version")
+  end
+end
