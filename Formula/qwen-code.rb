1bd714fc9cd6a2d1276ee4936c2109550c411a3f
MoonFruit <dkmoonfruit@gmail.com>
2025-08-20 17:16:33 +0800

    qwen-code 0.0.7
    Closes #262.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/qwen-code.rb
+++ Formula/qwen-code.rb
@@ -1,8 +1,8 @@
 class QwenCode < Formula
   desc "AI-powered command-line workflow tool for developers"
   homepage "https://github.com/QwenLM/qwen-code"
-  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.0.5.tgz"
-  sha256 "1b92595e24bd8648eff58b3c5e08d3b082536b9f3cc5a3beee64d0c543a612ef"
+  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.0.7.tgz"
+  sha256 "0c469ae2e122b822013c08e21c23a03f1bedb0d5e5fbcdb0323027ca9b1ffb4d"
   license "Apache-2.0"
 
   bottle do
