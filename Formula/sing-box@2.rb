c8f6729c106434df720a02704ceb763e4b0c1434
MoonFruit <dkmoonfruit@gmail.com>
2025-09-03 10:49:50 +0800

    sing-box@2: 1.13.0-alpha.8 (new formula)
    Closes #267.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

---
+++ Formula/sing-box@2.rb
@@ -0,0 +1,90 @@
+class SingBoxAT2 < Formula
+  desc "Universal proxy platform"
+  homepage "https://sing-box.sagernet.org"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.13.0-alpha.8.tar.gz"
+  sha256 "f199b4339c42304e6d85e8ae7d15a2153caeb75665d6bb66ad32d853650daa6c"
+  license "GPL-3.0-or-later"
+  head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
+
+  livecheck do
+    url :stable
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/sagernet/sing-box/constant.Version=#{version} -buildid= -checklinkname=0"
+    tags = %w[
+      with_acme
+      with_clash_api
+      with_dhcp
+      with_gvisor
+      with_quic
+      with_tailscale
+      with_utls
+      with_wireguard
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"sing-box", tags: tags.join(",")), "./cmd/sing-box"
+    generate_completions_from_executable(bin/"sing-box", "completion")
+  end
+
+  service do
+    run [opt_bin/"sing-box", "run", "--config", etc/"sing-box/config.json", "--directory", var/"lib/sing-box"]
+    run_type :immediate
+    keep_alive true
+  end
+
+  test do
+    ss_port = free_port
+    (testpath/"shadowsocks.json").write <<~JSON
+      {
+        "inbounds": [
+          {
+            "type": "shadowsocks",
+            "listen": "::",
+            "listen_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    JSON
+    server = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json" }
+
+    sing_box_port = free_port
+    (testpath/"config.json").write <<~JSON
+      {
+        "inbounds": [
+          {
+            "type": "mixed",
+            "listen": "::",
+            "listen_port": #{sing_box_port}
+          }
+        ],
+        "outbounds": [
+          {
+            "type": "shadowsocks",
+            "server": "127.0.0.1",
+            "server_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    JSON
+    system bin/"sing-box", "check", "-D", testpath, "-c", "config.json"
+    client = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", "config.json" }
+
+    sleep 3
+    begin
+      system "curl", "--socks5", "127.0.0.1:#{sing_box_port}", "github.com"
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
