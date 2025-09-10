4d0809a9c10a2d7c2252f2e12f0fcdc25ba9a45d
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2025-09-10 10:55:59 +0000

    codebuddy-code: add 1.0.8 bottle.

--- Formula/codebuddy-code.rb
+++ Formula/codebuddy-code.rb
@@ -5,6 +5,13 @@ class CodebuddyCode < Formula
   sha256 "e26eb244b7b44548ea3f60df221ecfb84f31831b248f6d50e20cb614144a9b42"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b1ffae221913879a1d4a60f532951a4169a8f9787ae245680e3a57c754d70050"
+    sha256 cellar: :any_skip_relocation, ventura:       "3ae5912e158c6aeb06f0559b4060521d32f0d3d52797c5005736d5c31e4049f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbdb0508b5f7dff7068347e30498aa1fc3c9f7a181803c1cb7df73a86b1cd505"
+  end
+
   depends_on "node"
 
   def install
