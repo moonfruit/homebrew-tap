f768707a42ffb82fb3d7ea07151f35e1ff1175c8
MoonFruit <dkmoonfruit@gmail.com>
2025-09-15 15:18:36 +0800

    bug fix

--- .lazy.lua
+++ .lazy.lua
@@ -1,28 +1,28 @@
 print("Loading astrolsp.lua")
 ---@type LazySpec
 return {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-        servers = {
-            "rubocop",
-            "sorbet",
-        },
-        ---@diagnostic disable: missing-fields
-        config = {
-            ruby_lsp = {
-                capabilities = {
-                    general = {
-                        positionEncodings = { "utf-16" }
-                    },
-                },
-            },
-            rubocop = {
-                cmd = { "brew", "rubocop", "--lsp" },
-            },
-            sorbet = {
-                cmd = { "brew", "typecheck", "--lsp" },
-            }
-        },
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    servers = {
+      "rubocop",
+      "sorbet",
     },
+    ---@diagnostic disable: missing-fields
+    config = {
+      ruby_lsp = {
+        capabilities = {
+          general = {
+            positionEncodings = { "utf-16" },
+          },
+        },
+      },
+      rubocop = {
+        cmd = { "brew", "rubocop", "--lsp" },
+      },
+      sorbet = {
+        cmd = { "brew", "typecheck", "--lsp" },
+      },
+    },
+  },
 }
