d0a4e39c1d84b46303eba914a80145a194581d83
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2025-09-16 06:28:38 +0000

    geo: update 1.1_2 bottle.

--- Formula/geo.rb
+++ Formula/geo.rb
@@ -9,9 +9,9 @@ class Geo < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50d3698057d523fba2fe48bf40d3d4306cc8ac5cc3088dda48bdb1c560c03db6"
-    sha256 cellar: :any_skip_relocation, ventura:       "9085ba2b7f7bc744472fcf57e9447ffd8923ce97e175d6dcc99f14e8bf77645f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce128bf6501a2bb7c7e1555fb6d3e4e1c72e82f0f31a6196dea57fab91752f74"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e63ee807f38e1c3ab3033e5404fd0336750f3cdaeb0b60b6b4ca4a919570d4bd"
+    sha256 cellar: :any_skip_relocation, ventura:      "5b7d1148942e9d9de332e3e3ecb324c60f45e78f5eb0662df6ed70ee7c89f2c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ee57a8204d3b43391b3bd99b338fd4f6710d8f6f68cf17230f5236cbe2f02ad6"
   end
 
   depends_on "go" => :build
