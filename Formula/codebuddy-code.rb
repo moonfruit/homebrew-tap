5da5bcfc3df493d6fffded1cd7a84b162fedc0d2
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2025-09-16 02:48:18 +0000

    codebuddy-code: update 1.0.17 bottle.

--- Formula/codebuddy-code.rb
+++ Formula/codebuddy-code.rb
@@ -7,9 +7,9 @@ class CodebuddyCode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eaf7d3f673a6c2024cb1c9ef13c654cacdd6260a5ad98541aaa359ca568b6720"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80800864a7948161d3216e3f0429efc54784dc2bff26f7a91feaff15271ea6e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "922465863d350cc19534acfc20badfab3dc1fada55485b87f84d226778c401f5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "84f680d6a369d80739d4ff9dce456be540b03200e31e9a8ef36b72f45400c300"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc9b582ff4bb9918b4dfdb8ed720b8bb23988f2ddba742babab6e5c1cfa36975"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07220d134c9f75d8548c75d2a90a2e112fa82f47e45e021c2b2a9d0b5fa27570"
   end
 
   depends_on "node"
