0b7d0f748303a3a469ac31fb2a6b619a3e553986
MoonFruit <dkmoonfruit@gmail.com>
2025-09-26 13:45:33 +0800

    codebuddy-code: revision bump
    Closes #312.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/codebuddy-code.rb
+++ Formula/codebuddy-code.rb
@@ -4,6 +4,7 @@ class CodebuddyCode < Formula
   url "https://registry.npmjs.org/@tencent-ai/codebuddy-code/-/codebuddy-code-1.3.0.tgz"
   sha256 "4595a11e4f8f71ee911c0e45e719cceb33dbb4aa479c8b582df2f0d7a1f77b09"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
@@ -16,8 +17,24 @@ class CodebuddyCode < Formula
 
   def install
     system "npm", "install", *std_npm_args
-    rm(Dir[libexec/"**/x64-darwin/**"]) if OS.mac? && Hardware::CPU.arm?
-    rm(Dir[libexec/"**/arm64-linux/**"]) if OS.linux? && Hardware::CPU.intel?
+    rm(Dir[libexec/"**/x64-win32/**"])
+    if OS.mac?
+      rm(Dir[libexec/"**/arm64-linux/**"])
+      rm(Dir[libexec/"**/x64-linux/**"])
+      if Hardware::CPU.arm?
+        rm(Dir[libexec/"**/x64-darwin/**"])
+      elsif Hardware::CPU.intel?
+        rm(Dir[libexec/"**/arm64-darwin/**"])
+      end
+    elsif OS.linux?
+      rm(Dir[libexec/"**/arm64-darwin/**"])
+      rm(Dir[libexec/"**/x64-darwin/**"])
+      if Hardware::CPU.arm?
+        rm(Dir[libexec/"**/x64-linux/**"])
+      elsif Hardware::CPU.intel?
+        rm(Dir[libexec/"**/arm64-linux/**"])
+      end
+    end
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
