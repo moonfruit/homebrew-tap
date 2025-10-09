623c4ac46341f09218ed896a79f85bfc94a81fc0
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2025-10-09 08:05:32 +0000

    im-select: add 1.0.0 bottle.

--- Formula/im-select.rb
+++ Formula/im-select.rb
@@ -5,6 +5,12 @@ class ImSelect < Formula
   sha256 "58ec2bf0f411a918ecab44204e069edaa5ed463320a8eca9e53be06e7ab909db"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "13fbb8d961a1ab72e6d5cad590b481d4cebe227a7e82f6866f9c155bbd1abe7c"
+    sha256 cellar: :any_skip_relocation, sequoia:     "3f5b01831d4d843d6518677836db40870437a6152bf6e6e7d50add375866e2d3"
+  end
+
   depends_on :macos
 
   def install
