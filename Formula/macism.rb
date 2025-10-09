728283fb63b441ae14e9d9c0acb3fc8ca07904ff
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2025-10-09 08:00:53 +0000

    macism: add 3.0.10 bottle.

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -5,6 +5,12 @@ class Macism < Formula
   sha256 "e333c5f158452494e159a9b8d848b14c72876ae891cdf83981f57f4fcf045e1f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "525ab0198ee0c2bd42bb72809112a964912c14d8658fd4ce65ff4baf0b3db019"
+    sha256 cellar: :any_skip_relocation, sequoia:     "adb06c9247842604b0d297f5576c49bcd7f7edc8191bf252c69ef7c3d5409f6d"
+  end
+
   depends_on :macos
 
   def install
