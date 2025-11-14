d9f1730a26a0f5baf6b5d88fef09571dfe73d57d
MoonFruit <dkmoonfruit@gmail.com>
2025-11-14 17:21:35 +0800

    font-source-han-sans: 2.005R

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,8 +1,8 @@
 cask "font-source-han-sans" do
-  version "2.004R"
-  sha256 "6f59118a9adda5a7fe4e9e6bb538309f7e1d3c5411f9a9d32af32a79501b7e4f"
+  version "2.005R"
+  sha256 "a024cf1759494847cd47aae4379bcb3dc530017c709f3f503ee0ed918dd92952"
 
-  url "https://github.com/adobe-fonts/source-han-sans/releases/download/#{version}/SourceHanSans.ttc.zip"
+  url "https://github.com/adobe-fonts/source-han-sans/releases/download/#{version}/01_SourceHanSans.ttc.zip"
   name "Source Han Sans"
   desc "OpenType Pan-CJK fonts"
   homepage "https://github.com/adobe-fonts/source-han-sans"
