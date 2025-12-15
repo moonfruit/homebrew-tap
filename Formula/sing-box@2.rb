3dbe8a38f78c94a987c23af914d5d69300c55339
MoonFruit <dkmoonfruit@gmail.com>
2025-12-15 11:17:46 +0800

    sing-box@2 1.13.0-alpha.28

--- Formula/sing-box@2.rb
+++ Formula/sing-box@2.rb
@@ -1,8 +1,8 @@
 class SingBoxAT2 < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org/"
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.13.0-alpha.27.tar.gz"
-  sha256 "3af0856ed30eb2aace3bfb421d1e1c45ddc34213b87334d15b8e376c8043dfde"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.13.0-alpha.28.tar.gz"
+  sha256 "0fcdb2dc34600116de60954a9a2671bd3a7a92b7a15b54e49eee15e3d866d903"
   license "GPL-3.0-or-later"
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
