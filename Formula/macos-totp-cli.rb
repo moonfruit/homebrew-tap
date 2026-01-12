94f995a7c4d735ecb0d6fcb4ab87ad8d8f57b75e
MoonFruit <dkmoonfruit@gmail.com>
2026-01-12 16:20:16 +0800

    macos-totp-cli: update build script
    Closes #426.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/macos-totp-cli.rb
+++ Formula/macos-totp-cli.rb
@@ -4,6 +4,7 @@ class MacosTotpCli < Formula
   url "https://github.com/simnalamburt/macos-totp-cli/archive/refs/tags/v1.1.3.tar.gz"
   sha256 "62f6febc7c5d6d6892d07aa39e383c2c4a26dcf985a00f89728b27983ee27079"
   license any_of: ["Apache-2.0", "MIT"]
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
@@ -14,10 +15,8 @@ class MacosTotpCli < Formula
   depends_on :macos
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w", output: "totp")
-    bin.install "totp"
-    ENV.prepend_path "PATH", bin
-    generate_completions_from_executable("totp", "completion", base_name: "totp")
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"totp")
+    generate_completions_from_executable(bin/"totp", shell_parameter_format: :cobra)
   end
 
   test do
