6e5e540c92289b2cb55f693d39126b699b1e9c83
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-01-28 02:22:59 +0000

    wlp-webprofile10: add 26.0.0.1 bottle.

--- Formula/wlp-webprofile10.rb
+++ Formula/wlp-webprofile10.rb
@@ -9,6 +9,12 @@ class WlpWebprofile10 < Formula
     regex(/wlp-webProfile10[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8224b567a7ec49e257b892d0a09d22082f75464c896e8570d7b34d1bfc2cca56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "69baf6f5280469782addf509fde7da5e2e34bf4518f044723857dc2ce5e71900"
+  end
+
   depends_on "openjdk"
 
   def install
