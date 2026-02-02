d0ba3f971023a71a3ef3efa282260513b4fd8365
MoonFruit <dkmoonfruit@gmail.com>
2026-02-02 10:12:22 +0800

    sing-box-ref1nd 1.13.0-beta.8-reF1nd.1
    Closes #449.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/sing-box-ref1nd.rb
+++ Formula/sing-box-ref1nd.rb
@@ -1,9 +1,9 @@
 class SingBoxRef1nd < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org/"
-  url "https://github.com/reF1nd/sing-box/archive/refs/tags/v1.13.0-beta.7-reF1nd.tar.gz"
-  version "1.13.0-beta.7-reF1nd"
-  sha256 "be395517992dcc42af323c27454b4906ae15c53d68ab9410369878ba5ac7aa24"
+  url "https://github.com/reF1nd/sing-box/archive/refs/tags/v1.13.0-beta.8-reF1nd.1.tar.gz"
+  version "1.13.0-beta.8-reF1nd.1"
+  sha256 "7bb4f4cc0e37cae797e7a94ecf137ef094e9891ddf33de212dc372c6d7b3cba0"
   license "GPL-3.0-or-later"
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
