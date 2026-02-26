17593d154b5cf150d836e8e4cebc32f65edb4f94
MoonFruit <dkmoonfruit@gmail.com>
2026-02-26 09:59:38 +0800

    codebuddy-code 2.51.2
    Closes #469.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/codebuddy-code.rb
+++ Formula/codebuddy-code.rb
@@ -1,8 +1,8 @@
 class CodebuddyCode < Formula
   desc "AI-powered command-line workflow tool for developers"
   homepage "https://cnb.cool/codebuddy/codebuddy-code"
-  url "https://registry.npmjs.org/@tencent-ai/codebuddy-code/-/codebuddy-code-2.51.0.tgz"
-  sha256 "fc27c140a647be425fb6b6f4b53c92e994466a59c54921dcf20ed938ee031051"
+  url "https://registry.npmjs.org/@tencent-ai/codebuddy-code/-/codebuddy-code-2.51.2.tgz"
+  sha256 "a518d850a73b11f6da88ff7c8cbe0e8cd813f7d1b11b5762b187e6f4bc533f48"
   license "MIT"
 
   bottle do
