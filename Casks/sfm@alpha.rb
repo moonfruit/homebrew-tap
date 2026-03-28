17c819747c079aec0083c3de59aaac1506af2922
MoonFruit <dkmoonfruit@gmail.com>
2026-03-28 23:05:39 +0800

    sfm: 1.14.0-alpha.7 (new cask)

---
+++ Casks/sfm@alpha.rb
@@ -0,0 +1,31 @@
+cask "sfm@alpha" do
+  arch arm: "Apple", intel: "Intel"
+
+  version "1.14.0-alpha.7"
+  sha256 arm:   "251810ca7e5ab442b20fff37714b4d99f804106cf356786225457e6d113dba49",
+         intel: "36998bedda5fb4c401278fdd13ca623c5d3a4e9b8ca7e87ea20d05b54aa36a54"
+
+  url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-#{arch}.pkg",
+      verified: "github.com/SagerNet/sing-box/"
+  name "SFM alpha"
+  desc "Standalone client for sing-box, the universal proxy platform"
+  homepage "https://sing-box.sagernet.org/"
+
+  livecheck do
+    regex(/^v?(\d+(?:\.\d+)+(?:[._-](?:alpha|beta)[._-]?\d+)?)$/i)
+  end
+
+  depends_on macos: ">= :ventura"
+
+  pkg "SFM-#{version}-#{arch}.pkg"
+
+  uninstall quit:       "io.nekohasekai.sfavt.standalone",
+            login_item: "SFM",
+            pkgutil:    "io.nekohasekai.sfavt.standalone"
+
+  zap trash: [
+    "~/Library/Application Scripts/287TTNZF8L.io.nekohasekai.sfavt",
+    "~/Library/Group Containers/287TTNZF8L.io.nekohasekai.sfavt",
+    "~/Library/Preferences/io.nekohasekai.sfavt.standalone.plist",
+  ]
+end
