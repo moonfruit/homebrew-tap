eece27fef9f967dfddef4a3a4f4d1fe83b9b4f93
MoonFruit <dkmoonfruit@gmail.com>
2026-04-11 00:11:04 +0800

    inkos: 1.1.1 (new formula)
    Closes #526.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

---
+++ Formula/inkos.rb
@@ -0,0 +1,18 @@
+class Inkos < Formula
+  desc "Autonomous novel writing cli ai agent"
+  homepage "https://github.com/Narcooo/inkos"
+  url "https://registry.npmjs.org/@actalk/inkos/-/inkos-1.1.1.tgz"
+  sha256 "72dd3898ec9902245c6a22bcb236ddf082daf3b2294142ab4323f20f68ca5d11"
+  license "AGPL-3.0-only"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/inkos --version")
+  end
+end
