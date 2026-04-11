8fb099bf307fbe99d649d7cf44d287d411ac0e46
MoonFruit <dkmoonfruit@gmail.com>
2026-04-12 02:01:09 +0800

    gmcurl: 1.0.3,7.88.1

--- Casks/gmcurl.rb
+++ Casks/gmcurl.rb
@@ -1,13 +1,14 @@
 cask "gmcurl" do
   arch arm: "aarch64", intel: "x64"
+  os macos: "macos", linux: "linux"
 
-  version "1.0.2"
+  version "1.0.3,7.88.1"
   sha256 :no_check
 
-  url "https://curl.gmssl.cn/down/gmcurl_macos_#{arch}"
+  url "https://curl.gmssl.cn/down/gmcurl_#{os}_#{arch}"
   name "gmcurl"
   desc "CURL with TLCP support"
   homepage "https://curl.gmssl.cn/"
 
-  binary "gmcurl_macos_#{arch}", target: "gmcurl"
+  binary "gmcurl_#{os}_#{arch}", target: "gmcurl"
 end
