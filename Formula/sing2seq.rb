54d7ec7b96a492412932a0bd7562d574c88642d5
MoonFruit <dkmoonfruit@gmail.com>
2026-04-13 16:35:47 +0800

    sing2seq 1.0.2
    Closes #535.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/sing2seq.rb
+++ Formula/sing2seq.rb
@@ -1,8 +1,8 @@
 class Sing2seq < Formula
   desc "Transporter used to send sing-box logs to seq"
   homepage "https://github.com/moonfruit/sing2seq"
-  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "7064dd1d4d15c19a629fefe586cfeab2f6cca9b431722d5c0fec609cf6674daf"
+  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "9f5ec90f5bd5a4d0434c6053a0e7b44f014b33352bf2ad39a423afd244673049"
   license "MIT"
 
   bottle do
@@ -12,10 +12,18 @@ class Sing2seq < Formula
   end
 
   depends_on "go" => :build
+  depends_on "bash"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
     bin.install "sing-box-wrap.sh"
+  end
+
+  def post_install
+    inreplace bin/"sing-box-wrap.sh" do |s|
+      s.gsub! "#!/usr/bin/env bash", "#!#{Formula["bash"].opt_bin}/bash"
+      s.gsub! ': "${SING2SEQ:=}"', ": \"${SING2SEQ:=#{opt_bin}/sing2seq}\""
+    end
   end
 
   test do
