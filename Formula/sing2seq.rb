01f599b861e808cfc74d5de55510aabc637c1b2d
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-04-13 07:20:52 +0000

    sing2seq: add 1.0.1 bottle.

--- Formula/sing2seq.rb
+++ Formula/sing2seq.rb
@@ -5,6 +5,12 @@ class Sing2seq < Formula
   sha256 "7064dd1d4d15c19a629fefe586cfeab2f6cca9b431722d5c0fec609cf6674daf"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "c980114781d4f460f1d5ea8a4a51ab1f4921e76775975ed2719d6cf8a6892ce2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dec7262590eeb58c3034ec9d4f7929cce7944d894838810aef9b1586798d26c5"
+  end
+
   depends_on "go" => :build
 
   def install
