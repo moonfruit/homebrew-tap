45194cb903930bc3a837bd6d5c49e221966db333
MoonFruit <dkmoonfruit@gmail.com>
2026-04-14 14:48:39 +0800

    sing2seq 1.1.0

--- Formula/sing2seq.rb
+++ Formula/sing2seq.rb
@@ -1,8 +1,8 @@
 class Sing2seq < Formula
   desc "Transporter used to send sing-box logs to seq"
   homepage "https://github.com/moonfruit/sing2seq"
-  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "bf23702f67dd160347643797101974a9b650793ff9c7f8ba978ce5c0e01441a7"
+  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "354c337c383be449d269a41235f3329056476cb840ba0012a1b28b1945c414b9"
   license "MIT"
 
   bottle do
@@ -12,21 +12,12 @@ class Sing2seq < Formula
   end
 
   depends_on "go" => :build
-  depends_on "bash"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
-    bin.install "sing-box-wrap.sh"
-  end
-
-  def post_install
-    inreplace bin/"sing-box-wrap.sh" do |s|
-      s.gsub! "#!/usr/bin/env bash", "#!#{Formula["bash"].opt_bin}/bash"
-      s.gsub! ': "${SING2SEQ:=}"', ": \"${SING2SEQ:=#{opt_bin}/sing2seq}\""
-    end
   end
 
   test do
-    assert_match "sing2seq #{version}", shell_output("#{bin}/sing2seq -version")
+    assert_match "sing2seq version #{version}", shell_output("#{bin}/sing2seq --version")
   end
 end
