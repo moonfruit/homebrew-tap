100612e8500c452553dc13fb6693adb88e3d0c14
MoonFruit <dkmoonfruit@gmail.com>
2026-04-24 13:58:49 +0800

    sfm@alpha 1.14.0-alpha.18

--- Casks/sfm@alpha.rb
+++ Casks/sfm@alpha.rb
@@ -1,11 +1,11 @@
 cask "sfm@alpha" do
   arch arm: "Apple", intel: "Intel"
 
-  version "1.14.0-alpha.17"
-  sha256 arm:          "8e5c292493a7ce1061e87bdab293ab7da426330f674f0ae2b2c74ef2b1b3c7f3",
-         intel:        "a5f7a2d52b68de2adb4fd96673f519a037ebdcf3247e6cc866c66cfb7ca390ee",
-         arm64_linux:  "8e5c292493a7ce1061e87bdab293ab7da426330f674f0ae2b2c74ef2b1b3c7f3",
-         x86_64_linux: "a5f7a2d52b68de2adb4fd96673f519a037ebdcf3247e6cc866c66cfb7ca390ee"
+  version "1.14.0-alpha.18"
+  sha256 arm:          "669e61ff3f9d924d229bb8a5da24fabc748bcfca6252b47e5b5d27e838f1875b",
+         intel:        "d463e99de81943ceffefd842a6abc9443cc1943640dd70509d0cfd01e13de998",
+         arm64_linux:  "669e61ff3f9d924d229bb8a5da24fabc748bcfca6252b47e5b5d27e838f1875b",
+         x86_64_linux: "d463e99de81943ceffefd842a6abc9443cc1943640dd70509d0cfd01e13de998"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-#{arch}.pkg",
       verified: "github.com/SagerNet/sing-box/"
