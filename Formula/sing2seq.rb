933adcc7d02c5c414b82e8353ebadf3f684af73e
MoonFruit <dkmoonfruit@gmail.com>
2026-05-06 10:32:37 +0800

    sing2seq 1.3.0

--- Formula/sing2seq.rb
+++ Formula/sing2seq.rb
@@ -1,8 +1,8 @@
 class Sing2seq < Formula
   desc "Transporter used to send sing-box logs to seq"
   homepage "https://github.com/moonfruit/sing2seq"
-  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "89920c783f9ee9ff5914b72c48ab8e68430f88911bb5a45573ae86e32a1b01e1"
+  url "https://github.com/moonfruit/sing2seq/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "91a1178b81adf1b5c6a089ce86c209f709e135f2ad8b2e98244e6a4b97903cfd"
   license "MIT"
 
   bottle do
@@ -14,7 +14,7 @@ class Sing2seq < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/sing2seq"
     generate_completions_from_executable(bin/"sing2seq", shell_parameter_format: :cobra)
   end
 
