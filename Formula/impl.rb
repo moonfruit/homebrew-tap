d2e71795f71412bb7a1a4605414a30176dd56235
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-05-09 15:48:13 +0000

    impl: update 1.5.0 bottle.

--- Formula/impl.rb
+++ Formula/impl.rb
@@ -7,7 +7,11 @@ class Impl < Formula
   head "https://github.com/josharian/impl.git", branch: "main"
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "bdd09c5d53f8e21b691e034c93d15d16e3225602b31c62674e9cc55995df5600"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "5d545902f1955d87484f6c8da67b69955536cc59a758c1df07539e9ca0adfbae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c63557c8bc6b494d88ffbac39914464321d73f7051025c0a3b3bf8d096f8d6da"
   end
 
   depends_on "go"
