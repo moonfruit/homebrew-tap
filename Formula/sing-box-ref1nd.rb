28409415075bd782a999ebd127401ecad3120255
MoonFruit <dkmoonfruit@gmail.com>
2026-05-09 18:32:09 +0800

    sing-box-ref1nd: enable naive outbound
    Closes #594.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/sing-box-ref1nd.rb
+++ Formula/sing-box-ref1nd.rb
@@ -5,6 +5,7 @@ class SingBoxRef1nd < Formula
   version "1.14.0-alpha.21-reF1nd"
   sha256 "5fdc43326a9063c4ddfa573c9757c31d88cbc4bc0b478f6117b66f5a859a0085"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/reF1nd/sing-box.git", branch: "reF1nd-dev-next"
 
   livecheck do
@@ -12,22 +13,27 @@ class SingBoxRef1nd < Formula
     regex(/^v(\d(?:\.\d+)+(-\w+(?:\.\d+)?)?-reF1nd(?:\.\d+)?)$/i)
   end
 
-  bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "5e71d490112ba2602d8e2aad5225d4b716a17e1a4ba29c5cdcd1d6c5dc98accd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "f94809cebad32b2394dffa5334c3e564bfacde97e82545e085c04a71aefe4f86"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "98a19d24fd2aa35b2715e06e14063b507e81ab86f378b9c53c077ca7df273b31"
-  end
-
   keg_only :versioned_formula
 
   depends_on "go" => :build
 
+  on_linux do
+    depends_on "lld" => :build
+    depends_on "llvm" => :build
+  end
+
   def install
-    ldflags = "-X github.com/sagernet/sing-box/constant.Version=#{version} " \
-              "#{(buildpath/"release/LDFLAGS").read.strip} -s -w -buildid="
-    tags = (buildpath/"release/DEFAULT_BUILD_TAGS_OTHERS").read.strip
+    tags = File.read("release/DEFAULT_BUILD_TAGS").strip.split(",")
+    ldflags_shared = File.read("release/LDFLAGS").strip
+
+    if OS.linux?
+      ENV["CC"] = Formula["llvm"].opt_bin/"clang"
+      ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
+      ENV["CGO_ENABLED"] = "1"
+      ENV["CGO_LDFLAGS"] = "-fuse-ld=#{Formula["lld"].opt_bin}/ld.lld"
+    end
+
+    ldflags = "-s -w -X github.com/sagernet/sing-box/constant.Version=#{version} #{ldflags_shared} -buildid="
     system "go", "build", *std_go_args(ldflags:, output: bin/"sing-box", tags:), "./cmd/sing-box"
     generate_completions_from_executable(bin/"sing-box", shell_parameter_format: :cobra)
   end
@@ -53,7 +59,7 @@ class SingBoxRef1nd < Formula
         ]
       }
     JSON
-    server = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json" }
+    server = spawn bin/"sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json"
 
     sing_box_port = free_port
     (testpath/"config.json").write <<~JSON
@@ -77,15 +83,15 @@ class SingBoxRef1nd < Formula
       }
     JSON
     system bin/"sing-box", "check", "-D", testpath, "-c", "config.json"
-    client = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", "config.json" }
+    client = spawn bin/"sing-box", "run", "-D", testpath, "-c", "config.json"
 
-    sleep 3
     begin
+      sleep 3
       system "curl", "--socks5", "127.0.0.1:#{sing_box_port}", "github.com"
     ensure
-      Process.kill 9, server
+      Process.kill "TERM", server
+      Process.kill "TERM", client
       Process.wait server
-      Process.kill 9, client
       Process.wait client
     end
   end
