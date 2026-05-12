4ae5f742c3d6e153919c242082c2df5617dd1345
MoonFruit <dkmoonfruit@gmail.com>
2026-05-12 22:33:31 +0800

    wlp-webprofile10 26.0.0.4
    Closes #604.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/wlp-webprofile10.rb
+++ Formula/wlp-webprofile10.rb
@@ -1,8 +1,8 @@
 class WlpWebprofile10 < Formula
   desc "Jakarta EE and MicroProfile application server (Jakarta EE Web Profile 10)"
   homepage "https://www.ibm.com/cloud/websphere-liberty"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/26.0.0.3/wlp-webProfile10-26.0.0.3.zip"
-  sha256 "d62deaf9dbad1dfd2503c72d3f8876e64c8e08d5a5caaf86d7809cf7d22671c5"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/26.0.0.4/wlp-webProfile10-26.0.0.4.zip"
+  sha256 "75513ac857a4e0810415a454ee175f6a594e1842ff235c0096f7b34b4e03e104"
 
   livecheck do
     url "https://www.ibm.com/support/pages/websphere-liberty-developers"
