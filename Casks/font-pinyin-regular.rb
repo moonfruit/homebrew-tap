d85e172ad0ba1986052c749b95f71de306e7e985
MoonFruit <dkmoonfruit@gmail.com>
2026-05-12 10:45:27 +0800

    font-pinyin-regular 1.10.2

--- Casks/font-pinyin-regular.rb
+++ Casks/font-pinyin-regular.rb
@@ -1,6 +1,6 @@
 cask "font-pinyin-regular" do
-  version "1.10.1"
-  sha256 "2d308f3ebd984281f39e894fdc91924da9102ebc0a1540e056edca43d4e5bd55"
+  version "1.10.2"
+  sha256 "03fce50e8b3143e9d71163e2b5101c1541cb63339c8b74739865a7600ca5349d"
 
   url "https://github.com/jaywcjlove/pinyin-font/releases/download/v#{version}/pinyin-regular.ttf"
   name "font-pinyin-regular"
