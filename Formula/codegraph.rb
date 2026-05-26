4f2d33ee051e33e22a1aaac96e33b4f3c37ae233
MoonFruit <dkmoonfruit@gmail.com>
2026-05-26 14:04:07 +0800

    codegraph 0.9.4 (new formula)
    Closes #643.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

---
+++ Formula/codegraph.rb
@@ -0,0 +1,18 @@
+class Codegraph < Formula
+  desc "Local-first code intelligence for AI agents via MCP"
+  homepage "https://github.com/colbymchenry/codegraph"
+  url "https://registry.npmjs.org/@colbymchenry/codegraph/-/codegraph-0.9.4.tgz"
+  sha256 "eb580f489a72e6700ada5066b909199b50c553472d5a7aef6967c073e10414be"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/codegraph --version")
+  end
+end
