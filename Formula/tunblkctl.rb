21032e013b241fbafa23429758b8a56df0b9901f
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-06-01 06:56:29 +0000

    tunblkctl: add 0.5.0 bottle.

--- Formula/tunblkctl.rb
+++ Formula/tunblkctl.rb
@@ -6,6 +6,13 @@ class Tunblkctl < Formula
   license "MIT"
   head "https://github.com/azhuchkov/tunblkctl.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "9d2c5c95c1b780b1970c2d760a85a650ad378a03c117e9123d3851a4a0bd4975"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "a01a84ad2ef67f7c9358ee0469d7b32eef37506727b6b079132f3ecc19523300"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0e3ac959a6f140e0a58d31dbc0715c1587ab447210e1e84ec038f28f698de5ae"
+  end
+
   def install
     prefix.install "libexec"
 
