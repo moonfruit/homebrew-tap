fbf0bc173db9db989f99f46b5944fcfe410ca440
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-06-01 03:25:06 +0000

    zenity-go: add 0.10.14 bottle.

--- Formula/zenity-go.rb
+++ Formula/zenity-go.rb
@@ -5,6 +5,13 @@ class ZenityGo < Formula
   sha256 "10ccfaa5e454048ee6383883f64e510ba98dd440c00e3951e5458a07052ee539"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "1235386780c23bf5f361e446cd9344d85032c682889cab58d375a06f63c81ccf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "d5e093f3a53fc0823276830a1e07701fbe7397307ac7230be61e6594bbf3cf59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0b8b2e23cbf0f5cff4b004469865529d230a8f5cf75573287923128dc02d2a0a"
+  end
+
   depends_on "go" => :build
 
   conflicts_with "zenity", because: "both install `zenity` binaries"
