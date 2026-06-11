a856c4f6a71abc2fb229b35a693951a5eaa422fb
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-06-11 07:33:57 +0000

    marksman-bundled: add 2026-02-08 bottle.

--- Formula/marksman-bundled.rb
+++ Formula/marksman-bundled.rb
@@ -6,6 +6,13 @@ class MarksmanBundled < Formula
   license "MIT"
   head "https://github.com/artempyanykh/marksman.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
+    sha256 cellar: :any, arm64_tahoe:  "7d8eac4deab655164d869495aa0c7e25c4996f08fa14815d2643072f86e063c9"
+    sha256 cellar: :any, arm64_linux:  "75f75b96db6a341dd0dfe58313d201fec5919b7a1cd2765df3d28155c53d809c"
+    sha256 cellar: :any, x86_64_linux: "1a0998f79b1e9286af8a5fb21ffe62b4262d4acee254bc13b48f1da093bdd49b"
+  end
+
   depends_on "dotnet@9" => :build # https://github.com/artempyanykh/marksman/pull/446
   depends_on "brotli"
 
