5e63fdd5681b38aed4ecd96b00c6305d5711760f
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-06-25 02:39:43 +0000

    officecli-bundled: update 1.0.120 bottle.

--- Formula/officecli-bundled.rb
+++ Formula/officecli-bundled.rb
@@ -7,9 +7,9 @@ class OfficecliBundled < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any, arm64_tahoe:  "1bc7d5674b1928616916ed6d300cae709d10a2162c40fa19737a2dde30bab459"
-    sha256 cellar: :any, arm64_linux:  "b0bafdc021c23e80d7712013ac02a153014da95c25621eb14528c262444de478"
-    sha256               x86_64_linux: "0f6aba2308fbd53eb2097289e1f299b3ab7cbcfedd0ca45ec951192f30226ed2"
+    sha256 cellar: :any, arm64_tahoe:  "b7345d8992299a3a9d228676e9340218d989c22eaeb9cad380dfbb894af67914"
+    sha256 cellar: :any, arm64_linux:  "73e8bb31726528e0726a671ad7c80878785e9382d385b584935669167638acc2"
+    sha256               x86_64_linux: "b9726d61cf3e99c9bdb92c7492af53e353178d841be2fbaacb22d6234ab37dab"
   end
 
   depends_on "dotnet" => :build
