0905667a96250f06d312bbcf3bb2dc1254595399
MoonFruit <dkmoonfruit@gmail.com>
2026-08-31 00:13:11 +0800

    sing-box-beta 1.14.0-rc.5

--- Formula/sing-box-beta.rb
+++ Formula/sing-box-beta.rb
@@ -1,8 +1,8 @@
 class SingBoxBeta < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org/"
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.14.0-rc.1.tar.gz"
-  sha256 "cb2ac9e2d9f4cdfde51fc634b7dc579675fc023053e2f57e5f13e0e9d19f3a47"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.14.0-rc.5.tar.gz"
+  sha256 "18dc5e658fa3121bbe1b7d719a789fa5c95464e9970a35b6db80376f2638b083"
   license "GPL-3.0-or-later"
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
