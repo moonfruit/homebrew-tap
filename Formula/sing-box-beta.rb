156c3c5e741a780a0d3d98a9becc0bb3738a23e5
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-08-31 01:34:36 +0000

    sing-box-beta: update 1.14.0-rc.5 bottle.

--- Formula/sing-box-beta.rb
+++ Formula/sing-box-beta.rb
@@ -12,9 +12,9 @@ class SingBoxBeta < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "cac6c1c720e9d8bc5f6a4ccca282def6958c5c68f4d49b1ba94f538ab0c67bc0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "cd71a9dd74d6ad96f184a687317931225a03d578c121b36b7a2993ed9e952267"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "77b3d8558063598c30d4a71ed0c0b46fa25a289fb0cb10e9bd0f294d2ff7f4b8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6765a1748ea10857602ec6dfc20ff5f72e3627397b0467dd3d21ac3a246f53cc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "934f36b48dccc13ac78a8b5a2fc0622f43aec2de6a823f64c7a8e8c5b01a6808"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "72a03b135bafa50d976fa6ec73bda8d3571fb555f48622d6142ee9a38e7c9e04"
   end
 
   keg_only :versioned_formula
