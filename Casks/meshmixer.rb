ebf14648365a48ea6288ea31716d3ac4f07042b2
MoonFruit <dkmoonfruit@gmail.com>
2026-09-02 11:23:04 +0800

    meshmixer: drop deprecated verified

--- Casks/meshmixer.rb
+++ Casks/meshmixer.rb
@@ -2,8 +2,7 @@ cask "meshmixer" do
   version "3.5.474"
   sha256 :no_check
 
-  url "https://web.archive.org/web/20190731155031/http://www.meshmixer.com/downloads/Autodesk_Meshmixer_v3p5_MacOS.pkg",
-      verified: "web.archive.org/"
+  url "https://web.archive.org/web/20190731155031/http://www.meshmixer.com/downloads/Autodesk_Meshmixer_v3p5_MacOS.pkg"
   name "meshmixer"
   desc "3D modeling software"
   homepage "https://meshmixer.org/"
