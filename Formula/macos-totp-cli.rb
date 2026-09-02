46cb9d434937ee60ab144932b57baaec3465b744
MoonFruit <dkmoonfruit@gmail.com>
2026-09-02 16:57:50 +0800

    macos-totp-cli 1.1.4

--- Formula/macos-totp-cli.rb
+++ Formula/macos-totp-cli.rb
@@ -1,10 +1,9 @@
 class MacosTotpCli < Formula
   desc "Simple TOTP CLI, powered by keychain of macOS"
   homepage "https://github.com/simnalamburt/macos-totp-cli"
-  url "https://github.com/simnalamburt/macos-totp-cli/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "62f6febc7c5d6d6892d07aa39e383c2c4a26dcf985a00f89728b27983ee27079"
+  url "https://github.com/simnalamburt/macos-totp-cli/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "c70057bfc159e79509c46ffa33af460844b7a1733765a22c4ac5faf6e20fdf10"
   license any_of: ["Apache-2.0", "MIT"]
-  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
