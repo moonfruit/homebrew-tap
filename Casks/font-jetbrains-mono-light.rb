2b1e2ecba636d4913eddb2da26b6a32474085c48
MoonFruit <dkmoonfruit@gmail.com>
2026-09-02 11:23:04 +0800

    font-jetbrains-mono-light: drop deprecated verified

--- Casks/font-jetbrains-mono-light.rb
+++ Casks/font-jetbrains-mono-light.rb
@@ -2,8 +2,7 @@ cask "font-jetbrains-mono-light" do
   version "2.304"
   sha256 "6f6376c6ed2960ea8a963cd7387ec9d76e3f629125bc33d1fdcd7eb7012f7bbf"
 
-  url "https://github.com/JetBrains/JetBrainsMono/releases/download/v#{version}/JetBrainsMono-#{version}.zip",
-      verified: "github.com/JetBrains/JetBrainsMono/"
+  url "https://github.com/JetBrains/JetBrainsMono/releases/download/v#{version}/JetBrainsMono-#{version}.zip"
   name "JetBrains Mono"
   desc "Typeface made for developers"
   homepage "https://www.jetbrains.com/lp/mono"
