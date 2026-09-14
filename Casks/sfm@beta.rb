0177d7e69d69dfb34a3c0ab84254dadd482efcd4
MoonFruit <dkmoonfruit@gmail.com>
2026-09-14 09:52:09 +0800

    sfm@beta 1.15.0-alpha.3 (#911)

--- Casks/sfm@beta.rb
+++ Casks/sfm@beta.rb
@@ -1,9 +1,9 @@
 cask "sfm@beta" do
   arch arm: "Apple", intel: "Intel"
 
-  version "1.15.0-alpha.2"
-  sha256 arm:   "ffd2b2ffeb3f2c05953093cd35d7d32a3f380c95e1348c65622edbd67dbc87c5",
-         intel: "cf965235f6985e6786de497fdb2b669dda2d7f0bc9b6f32eaccd8338457b3622"
+  version "1.15.0-alpha.3"
+  sha256 arm:   "cc7429a1a9d208cdafe37dfc38d4ea66f94d1ae16d9fcd2ccf28eb98b80453b1",
+         intel: "b908da8eba63d0156fbe3df0117cd75d7ab07843d16a0da6cb3200b4f9179d09"
 
   url "https://github.com/SagerNet/sing-box/releases/download/v#{version}/SFM-#{version}-#{arch}.pkg"
   name "SFM beta"
