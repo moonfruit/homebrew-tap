90e8bf99dd5a6985365713bae039064057ee355f
MoonFruit <dkmoonfruit@gmail.com>
2026-09-14 10:38:14 +0800

    p7zip-full 17.06 (new formula)
    - Replace `p7zip-all`, following the `ffmpeg-full` naming convention.

--- Formula/p7zip-full.rb
+++ Formula/p7zip-full.rb
@@ -1,16 +1,12 @@
-class P7zipAll < Formula
+class P7zipFull < Formula
   desc "7-Zip (high compression file archiver) implementation"
   homepage "https://github.com/p7zip-project/p7zip"
   url "https://github.com/p7zip-project/p7zip/archive/refs/tags/v17.06.tar.gz"
   sha256 "c35640020e8f044b425d9c18e1808ff9206dc7caf77c9720f57eb0849d714cd1"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
-  revision 1
 
-  bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8c1f2e1e4a240b60492086cff5024df1a526d6456ca440b8345bb861bb2a695b"
-    sha256 cellar: :any_skip_relocation, ventura:      "8799378f1ba0bb315f0d9557a3bf0370931052e9cabea31c9dd4bc8fc84c4f6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a6995a7cb4fa11cff52828c502af41998d485d3cf89bbf799090e9707e42a87c"
+  livecheck do
+    formula "p7zip"
   end
 
   keg_only :versioned_formula
