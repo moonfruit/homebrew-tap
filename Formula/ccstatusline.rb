8f6fa8a01323d78801fc82002272ca291e12dc39
MoonFruit <dkmoonfruit@gmail.com>
2026-09-17 11:45:18 +0800

    ccstatusline: rebuild 1
    Closes #933.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/ccstatusline.rb
+++ Formula/ccstatusline.rb
@@ -6,10 +6,7 @@ class Ccstatusline < Formula
   license "MIT"
 
   bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "2e62b2f8ce22c6f0fd61b620d7ce7a689a44d9ef7dbf257371dbdd6582b8dbd1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "bc1659e3948d922a9d032861d1f70894df93c00dbe125c24223e0491654a9ae5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "13605e7e07ec1a3a3fed2d2c8d6acf433693532b215040e45e76e45302743481"
+    rebuild 1
   end
 
   depends_on "node"
