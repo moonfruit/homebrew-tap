35b38659c9a99d9b414e09b16e89da296d8d1ce6
MoonFruit <dkmoonfruit@gmail.com>
2026-09-17 15:14:06 +0800

    codegraph: rebuild 2
    Closes #970.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/codegraph.rb
+++ Formula/codegraph.rb
@@ -6,12 +6,7 @@ class Codegraph < Formula
   license "MIT"
 
   bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_golden_gate: "220a7b404232d967efdcf6fe587e02c10a41bd57524717ada279eec78dbf5f56"
-    sha256 cellar: :any,                 arm64_tahoe:       "d41dac72c082927f39864d249b92b996cfba5b6de926d05d1b6c50ac01522f5c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "1681d8c9579242beaa5a1ac302812c35a1e4064a575ed8fbe2675a8b1e0ecc91"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "9f593414955070ca7a78be08462753644a1c6edec100e4481399b17469234a66"
+    rebuild 2
   end
 
   depends_on "node"
