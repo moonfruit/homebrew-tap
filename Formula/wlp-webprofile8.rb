1b7f8f49a9c3534ab45e5c0ffbe7567b858cca38
MoonFruit <dkmoonfruit@gmail.com>
2026-09-17 15:13:44 +0800

    wlp-webprofile8: rebuild 2

--- Formula/wlp-webprofile8.rb
+++ Formula/wlp-webprofile8.rb
@@ -10,12 +10,7 @@ class WlpWebprofile8 < Formula
   end
 
   bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f8d95cac6696689543e13f09c8ab77da01e8aee1130bc57114d2551d326f219f"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "7c531fb002c8b826187c117644da71c0f4c2e49b932196eff36c68001f84ef95"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "6103586da57a92f11a24a08d5e329f273ec6b6b5fd66cd16c4c048fc5dcb42de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "c72649ebc73d40d0a049076e69291e8674fd5c2b27a1c23be1e01133815fb82b"
+    rebuild 2
   end
 
   depends_on "openjdk"
