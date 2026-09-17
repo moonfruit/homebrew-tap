d1144b9c1f971d5a0244294b59b6f089cafa2760
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 04:21:14 +0000

    ngrrram: update 1.0.3_2 bottle.

--- Formula/ngrrram.rb
+++ Formula/ngrrram.rb
@@ -7,7 +7,12 @@ class Ngrrram < Formula
   revision 2
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "18d1727e5bbc03fe812e3d99fd43edd672617b00212681383cdb05e33f24c334"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "73cdc30e080ab6daf86fec403338cca70cafbccd73d2f4b3c679ac76bc4377dc"
+    sha256 cellar: :any,                 arm64_linux:       "256e6ef2b727cb69f1f9f28ee88d9366a954df93a3b84c1011f9b2ea8cb061b6"
+    sha256 cellar: :any,                 x86_64_linux:      "40c865020c1f7fa50373906ae2ab885ac06644a3d48737f2082f2618f1c2fff9"
   end
 
   depends_on "rust" => :build
