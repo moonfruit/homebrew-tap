8f0a1ba642779929840ca09173e68801e4283fdf
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 04:31:16 +0000

    tunblkctl: update 0.5.0 bottle.

--- Formula/tunblkctl.rb
+++ Formula/tunblkctl.rb
@@ -7,7 +7,12 @@ class Tunblkctl < Formula
   head "https://github.com/azhuchkov/tunblkctl.git", branch: "main"
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "19e360802d4f52fbfb7abd047be384372cb31cc90ea9e53a396a9f6a74fd0c0d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "7896327f24e0765052f06359c48a3a7cbfb030b6a7452296fdac413579bb4146"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "e798c663f09297f4f2a8fcc2bb86f22ec53c9dfe1d49815f41d66b5cae449610"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "88afc717e9ef44dccff914144c1c8e34cdfdc70093ab8820dc40b190bc14eab7"
   end
 
   def install
