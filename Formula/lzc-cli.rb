b0534479680d49e15b2bd721d2094773496cb54c
MoonFruit <dkmoonfruit@gmail.com>
2026-09-17 11:46:42 +0800

    lzc-cli: rebuild 1
    Closes #943.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/lzc-cli.rb
+++ Formula/lzc-cli.rb
@@ -6,10 +6,7 @@ class LzcCli < Formula
   license "ISC"
 
   bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    sha256 cellar: :any, arm64_tahoe:  "25ad3ad6132ddf587345e7204803e50f023d475adfc581d48be804795d9b3da9"
-    sha256 cellar: :any, arm64_linux:  "b332e4dfdf108e8f948b0803e08411056f2c75a34e33d9588876e722267810b6"
-    sha256 cellar: :any, x86_64_linux: "c3321ff798adc9a58834267f7330ff9c791ed6b27b7f879624967d52fa2a2245"
+    rebuild 1
   end
 
   depends_on "node"
