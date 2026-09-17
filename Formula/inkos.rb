0984d7b5b6d84e64c3dd06acd6e02c3d5d1a0b0c
MoonFruit <dkmoonfruit@gmail.com>
2026-09-17 15:14:15 +0800

    inkos: rebuild 2
    Closes #971.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/inkos.rb
+++ Formula/inkos.rb
@@ -6,12 +6,7 @@ class Inkos < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    root_url "https://ghcr.io/v2/moonfruit/bottle"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "45e4c4d6e4b2c94f4b16ec39db57cdadaae571087ab0711b435c13015789b428"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "6cca75a363f5e1c7f161a20dcff65dc176df10069c18ca9a1717c24fa1d07454"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "c7abf16ed9ba2daf888814516dd1f6d3de1b4b99999e2e0f1e29d915ffb45449"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "bab1d809953d8b3320c1a9dcdf9adbf8e6c502f05984c5d19578221ee3742145"
+    rebuild 2
   end
 
   depends_on "node"
