b9b260b625de1e2d550988c5f7b93500919ac12d
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 07:37:09 +0000

    lzc-cli: update 2.0.9 bottle.

--- Formula/lzc-cli.rb
+++ Formula/lzc-cli.rb
@@ -6,7 +6,12 @@ class LzcCli < Formula
   license "ISC"
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 2
+    sha256 cellar: :any, arm64_golden_gate: "fcd806dcfb52a12b887dfe4e9a30ce9a84c34686fa5ad6e0e03ebe1fccd7eefc"
+    sha256 cellar: :any, arm64_tahoe:       "fcd806dcfb52a12b887dfe4e9a30ce9a84c34686fa5ad6e0e03ebe1fccd7eefc"
+    sha256 cellar: :any, arm64_linux:       "bf927c5286838c9972cd758c59ea489f8864148c6e042ad74ff85410d36caed4"
+    sha256 cellar: :any, x86_64_linux:      "b497e1449204294a368a208680bba8885db40b90573828ae6ff04cc5615b8c3a"
   end
 
   depends_on "node"
