28e504961a444fc43fd3cbf8a9de4ddb9576c0ad
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 07:37:24 +0000

    inkos: update 1.8.0 bottle.

--- Formula/inkos.rb
+++ Formula/inkos.rb
@@ -6,7 +6,12 @@ class Inkos < Formula
   license "AGPL-3.0-only"
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "3d442db62da685b228d492c4b4f831118360e75c704061eef32f5e59f6b59266"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "3d442db62da685b228d492c4b4f831118360e75c704061eef32f5e59f6b59266"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "0fdc6e1bf671d393d3cead88159fbc1c662880440f609c227a57feec7c5ca2f8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "0fdc6e1bf671d393d3cead88159fbc1c662880440f609c227a57feec7c5ca2f8"
   end
 
   depends_on "node"
