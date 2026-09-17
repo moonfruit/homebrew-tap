efc328f7c77f5f001a590f9a5a8c05b6496dc5a1
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 07:39:05 +0000

    codegraph: update 1.6.0 bottle.

--- Formula/codegraph.rb
+++ Formula/codegraph.rb
@@ -6,7 +6,12 @@ class Codegraph < Formula
   license "MIT"
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 2
+    sha256 cellar: :any,                 arm64_golden_gate: "3340923cc991fbcdab4d26c0695d80abe905a046f308ca449337212be339e94b"
+    sha256 cellar: :any,                 arm64_tahoe:       "3340923cc991fbcdab4d26c0695d80abe905a046f308ca449337212be339e94b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "62d9d3211a7d7fbc460594ba39b30c1117f55fe1551cafff000512c906da2495"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "7c3c2774e25065ac35d2935cbc3c19a91eec13ae268458fe0a17fcdc8442a34d"
   end
 
   depends_on "node"
