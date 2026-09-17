d4d51b91e72ed68f20e66f3c6ac6a3753fd1701d
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-17 04:31:29 +0000

    p7zip-full: update 17.06 bottle.

--- Formula/p7zip-full.rb
+++ Formula/p7zip-full.rb
@@ -10,7 +10,12 @@ class P7zipFull < Formula
   end
 
   bottle do
+    root_url "https://ghcr.io/v2/moonfruit/bottle"
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "2922f126182f5539c8287683ffcf926917fb0a97a1ff7c05398057be74243b82"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "34fccdf777698c3f40eb92fda4c675c84a29061c4c1bae402041d0bc561715f7"
+    sha256 cellar: :any,                 arm64_linux:       "07c1ea2b6f3eb7b2f735433ec773b6000191e78aee4d089704629cae214520ea"
+    sha256 cellar: :any,                 x86_64_linux:      "fef113d45320efee705b24564a92ff1300e7617e16e43b250f7eda355baaaba2"
   end
 
   keg_only :versioned_formula
