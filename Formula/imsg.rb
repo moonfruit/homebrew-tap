a6141857bb25ac82e05f0481d45cd74946107b07
MoonFruit <dkmoonfruit@gmail.com>
2026-09-18 10:26:20 +0800

    imsg 0.15.6
    Closes #977.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/imsg.rb
+++ Formula/imsg.rb
@@ -1,8 +1,8 @@
 class Imsg < Formula
   desc "Send and read iMessage / SMS from the terminal"
   homepage "https://github.com/openclaw/imsg"
-  url "https://github.com/openclaw/imsg/archive/refs/tags/v0.15.5.tar.gz"
-  sha256 "440dcedd0dde57b32e48a54e634ae2db75c2592bed0479d5be3061e3e34517c2"
+  url "https://github.com/openclaw/imsg/archive/refs/tags/v0.15.6.tar.gz"
+  sha256 "f0df34e3a69f1e331fde285eaa99782c29520ef591a69f50fe16f0b70ba6d021"
   license "MIT"
 
   bottle do
