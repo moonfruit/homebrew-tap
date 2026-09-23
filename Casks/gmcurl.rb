89323d4e77a68bf87b5664530ca8184377fce870
MoonFruit <dkmoonfruit@gmail.com>
2026-09-23 15:06:23 +0800

    gmcurl: add livecheck and pin sha256

--- Casks/gmcurl.rb
+++ Casks/gmcurl.rb
@@ -2,8 +2,11 @@ cask "gmcurl" do
   arch arm: "aarch64", intel: "x64"
   os macos: "macos", linux: "linux"
 
-  version "1.0.3,7.88.1"
-  sha256 :no_check
+  version "1.0.3"
+  sha256 arm:          "845bee162a2b8205cd87c6fcac17955a3b4bfc305108e9e644771740e9e42757",
+         intel:        "a95b6d5e704a90f5b5dc3461fef1d6d677cba86e63997ff4acfcc15c34a89618",
+         arm64_linux:  "4d307ea153862963868d9408c1a1bfef1363432c14d2b877c069491595598d38",
+         x86_64_linux: "69c6ef0d5e8788b92e9c3c90b2faa6d680a645151bb1c1acaee5302d799217ba"
 
   on_macos do
     caveats <<~EOS
@@ -19,6 +22,11 @@ cask "gmcurl" do
   name "gmcurl"
   desc "CURL with TLCP support"
   homepage "https://curl.gmssl.cn/"
+
+  livecheck do
+    url "https://curl.gmssl.cn/txt/version.txt"
+    regex(/^V(\d+(?:\.\d+)+)/i)
+  end
 
   binary "gmcurl_#{os}_#{arch}", target: "gmcurl"
 end
