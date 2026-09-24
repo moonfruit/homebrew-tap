7aa00543894357aa7e60026cfe24db74f257fa4c
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-24 12:54:53 +0000

    sing-box-beta 1.15.0-alpha.8
    Closes #1000.
    
    Signed-off-by: MoonFruit <1070090+moonfruit@users.noreply.github.com>

--- Formula/sing-box-beta.rb
+++ Formula/sing-box-beta.rb
@@ -1,8 +1,8 @@
 class SingBoxBeta < Formula
   desc "Universal proxy platform"
   homepage "https://sing-box.sagernet.org/"
-  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.15.0-alpha.7.tar.gz"
-  sha256 "8cf923538935bd9b3a349219cdb11960ab3692e5dd10075ee32641130a247edc"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.15.0-alpha.8.tar.gz"
+  sha256 "758fc66abc47445a08fd8dd788fe0c39aae2f9cfa1e2c2bb30d788475e2e5828"
   license "GPL-3.0-or-later"
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
