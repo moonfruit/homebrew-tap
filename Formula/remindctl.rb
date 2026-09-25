eba93eac874f95a57a8f7b06041a466abb6b385f
MoonFruit <1070090+moonfruit@users.noreply.github.com>
2026-09-25 11:46:08 +0000

    remindctl: update 0.3.8 bottle.

--- Formula/remindctl.rb
+++ Formula/remindctl.rb
@@ -7,9 +7,8 @@ class Remindctl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "969fccdacd4dfae3f05dd8798dc26013c1e8a20a9b0110b0c06ae2ce800d5228"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "01dd1acedac398d9df27c1e340ed39fa705b77822123e6d3a252f7df27f7d9a6"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "7b1beb208953a2c32b25707d66007a741fb434683a658c638ddeea1e9cba2209"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "0cceede6db903c18b39c23e510cf1d629c3ce35760cec0b0451dfbb969dc99cd"
   end
 
   depends_on xcode: ["16.0", :build]
