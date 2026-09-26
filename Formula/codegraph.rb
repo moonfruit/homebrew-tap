0e679f9304299fb260a6210566716684b1893671
MoonFruit <dkmoonfruit@gmail.com>
2026-09-26 12:40:25 +0800

    codegraph: use node@24

--- Formula/codegraph.rb
+++ Formula/codegraph.rb
@@ -4,6 +4,7 @@ class Codegraph < Formula
   url "https://registry.npmjs.org/@colbymchenry/codegraph/-/codegraph-1.6.0.tgz"
   sha256 "832d2f608d2366ab48411a8123f91889c4a44407b8b87f3595276e29fd4e0129"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/moonfruit/bottle"
@@ -14,14 +15,31 @@ class Codegraph < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:      "7c3c2774e25065ac35d2935cbc3c19a91eec13ae268458fe0a17fcdc8442a34d"
   end
 
-  depends_on "node"
+  depends_on "node" => :test
+  # Upstream blocks Node >= 25 (V8 turboshaft WASM Zone OOM), see
+  # https://github.com/colbymchenry/codegraph/issues/81
+  depends_on "node@24"
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    # Use node@24 instead of the vendored Node runtime
+    bundle = libexec.glob("lib/node_modules/@colbymchenry/codegraph/node_modules/@colbymchenry/codegraph-*")
+                    .first.relative_path_from(libexec)
+    rm libexec/bundle/"node"
+    (bin/"codegraph").write <<~SH
+      #!/bin/bash
+      exec "#{formula_opt_bin("node@24")}/node" --liftoff-only --disable-warning=ExperimentalWarning \\
+        "#{opt_libexec}/#{bundle}/lib/dist/bin/codegraph.js" "$@"
+    SH
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/codegraph --version")
+
+    # When this fails, upstream supports the latest Node again:
+    # switch back to `depends_on "node"` and drop this check
+    entry = libexec.glob("lib/node_modules/@colbymchenry/codegraph/node_modules/@colbymchenry/codegraph-*").first
+    output = shell_output("#{formula_opt_bin("node")}/node #{entry}/lib/dist/bin/codegraph.js --version 2>&1", 1)
+    assert_match "Unsupported Node.js version", output
   end
 end
